b8eabbb26a61f3af6922339bc0f2d0562c8dbf12
j4yii <frozzen.brix@gmail.com>
2025-03-03 02:44:19 +0800

    styles, footer

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("signUp").addEventListener("click", function () {
-    document.querySelector(".container").classList.add("move-left");
-});
-
-document.getElementById("signIn").addEventListener("click", function () {
-    document.querySelector(".container").classList.remove("move-left");
-});
